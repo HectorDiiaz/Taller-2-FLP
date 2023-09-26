705e15efa967fb6e190cdc5f3cdff9af10ae4f8f
HectorDiiaz <hectorluis_d@hotmail.es>
2023-09-25 19:57:12 -0500

    Update punto 1

--- ejercicio1.rkt
+++ ejercicio1.rkt
@@ -1,6 +1,39 @@
 #lang racket
-;;fnc ::= 'FNC <numero> <clausula>
-;;clausula ::= <or-exp> {'and <or-exp>}*
-;;<or-exp> ::= <number> {'or <or-exp>}
+;;fnc       ::= 'FNC <numero> (<and-exp>+)
+;;<and-exp> ::= (<or-exp>+)
+;;<or-exp>  ::= (<number>+) 
 
 ;;Constructores
+(define fnc
+  (lambda (n lista)
+  (cond
+    [(empty? lista) empty]
+    [else (append (list 'FNC  n) (and-exp lista))]
+    )
+  ))
+
+(define or-exp
+  (
+   lambda(lista)
+    (cond
+      [(empty? lista) empty]
+      [(empty? (cdr lista)) ( list (car lista)) ]
+      [else (append
+             (list (car lista) 'OR) (or-exp (cdr lista)))]
+     )
+    ))
+
+(define and-exp
+  (
+   lambda(lista)
+    (cond
+      [(empty? lista) empty]
+      [(empty? (cdr lista)) ( list (or-exp (car lista)))]
+      [else (append
+             (list (or-exp (car lista)) 'AND)
+             (and-exp (cdr lista)))]
+     )
+    ))
+
+
+
